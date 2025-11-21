2bc44c02df594f4ce387abfa9f57970631549f86
Roysi Silva <roysi.ejemplo@gmail.com>
2025-11-21 14:18:31 -0500

    Conectar frontend con backend en la nube

--- frontend-reservas/src/config.js
+++ frontend-reservas/src/config.js
@@ -3,8 +3,8 @@
 // Aquí definimos las URLs de los servicios.
 // Si en el futuro subes esto a la nube, SOLO cambias este archivo.
 
-export const API_USUARIOS = 'http://127.0.0.1:8000';
-export const API_CANCHAS =  'http://127.0.0.1:8001';
-export const API_RESERVAS = 'http://127.0.0.1:8002';
+export const API_USUARIOS = 'https://reservas-usuarios.onrender.com';
+export const API_CANCHAS =  'https://reservas-canchas-um2v.onrender.com';
+export const API_RESERVAS = 'https://reservas-reservas.onrender.com';
 
 // Otras constantes globales pueden ir aquí
